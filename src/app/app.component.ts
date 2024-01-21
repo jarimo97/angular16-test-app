61f449a471a5d7e698ba0e9fe0dfb13577287a9c
jarimo97 <67900104+jarimo97@users.noreply.github.com>
2024-01-21 23:52:35 +0100

    add and use  angular library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TestModuleComponent } from './app.module'
 
 @Component({
   selector: 'app-root',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {NgxJacoboLib2Module, TestModuleComponent } from '@jarimo97/ngx-jacobo-lib2';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxJacoboLib2Module
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export { TestModuleComponent };
